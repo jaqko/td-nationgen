323e1b86b5cac2538ebd0f780772e476cac491f4
Jacko <35906674+jaqko@users.noreply.github.com>
2022-11-23 14:56:29 -0500

    Startpoint assigned to the rightmost x value

---
+++ Startlocation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startlocation : MonoBehaviour
+{
+    public string decidedLocation = "None";
+    void Start()
+    {
+        float number = Random.Range(0, 2);
+        if (Mathf.RoundToInt(number) == 0)
+            decidedLocation = "Newly Formed Island Nation";
+        else if (Mathf.RoundToInt(number) == 1)
+            decidedLocation = "Newly Formed Island Colony";
+        else if (Mathf.RoundToInt(number) == 0)
+            decidedLocation = "Newly Formed Island Rebellion";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
